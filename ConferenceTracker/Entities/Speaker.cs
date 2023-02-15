7f94836c2befaa5a0baab39ca771a98e0e128355
ebascoy <ebascoy@users.noreply.github.com>
2023-02-15 17:22:53 -0500

    Completed module

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,17 +4,46 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> result = new List<ValidationResult>();
+            if (EmailAddress != null
+                && EmailAddress.EndsWith("TechnologyLiveConference.com",
+                StringComparison.OrdinalIgnoreCase))
+            {
+                result.Add(new ValidationResult
+                    ("Technology Live Conference staff should not use their conference email addresses"));
+            }
+
+            return result;
+        }
+
+        [Required]
         public int Id { get; set; }
+        [Required]
         [Display(Name = "First name")]
+        [DataType(DataType.Text)]
+        [MaxLength(100)]
+        [MinLength(2)]
         public string FirstName { get; set; }
+        [Required]
         [Display(Name = "Last name")]
+        [DataType(DataType.Text)]
+        [MaxLength(100)]
+        [MinLength(2)]
         public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500)]
+        [MinLength(10)]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
     }
